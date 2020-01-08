ccabc229e9ccd89113a90d1e37762b5df3b9b027
Andrew Kirkwood <andrewkirkwood90@gmail.com>
2020-01-08 17:36:11 +0000

    panagram finished

--- pangram_finder/pangram_finder.js
+++ pangram_finder/pangram_finder.js
@@ -1,9 +1,14 @@
 const PangramFinder = function (phrase) {
   this.alphabet = 'qwertyuiopasdfghjklzxcvbnm'.split('');
+  this.phrase = phrase.toLowerCase().replace(/[^0-9a-z]/gi, ' ');
+
 }
 
 PangramFinder.prototype.isPangram = function () {
-
+  const result = this.alphabet.every((letter) => {
+    return this.phrase.includes(letter)
+  })
+  return result
 }
 
 module.exports = PangramFinder;
