ef1f3db5bababfc3e457f99790c198485b1195d2
Andrew Kirkwood <andrewkirkwood90@gmail.com>
2020-01-08 18:15:08 +0000

    isogram passed

--- isogram_finder/isogram_finder.js
+++ isogram_finder/isogram_finder.js
@@ -1,9 +1,17 @@
 const IsogramFinder = function (word) {
-
+  // this.alphabet = 'qwertyuiopasdfghjklzxcvbnm'.split('');
+  this.word = word.toLowerCase().split('')
 }
 
 IsogramFinder.prototype.isIsogram = function () {
 
+  const unresult = [...new Set(this.word)]
+
+  return unresult.length >= this.word.length
+  // return unresult
 }
+
+
+
 
 module.exports = IsogramFinder;
